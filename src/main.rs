59a37868da90f680829f56ba280efe682764875f
kruemmelbande <kai.scheufen@gmail.com>
2023-06-18 09:15:38 +0200

    Fixed some crashes, and made the ui nicer

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-const BOARD_SIZE:[usize;2] = [10,10];
 use rand::Rng;
 use std::io::Write;
-use std::io::{self, stdout};
-use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use std::io::{stdout};
+use crossterm::event::{self, Event, KeyCode};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
-
+const BOARD_SIZE:[usize;2] = [10,10];
 fn main() -> Result<(), Box<dyn std::error::Error>>  {
     //Snake in rust in the command line
 
     //Config:
 
-    let start_size= 3;
-    let wall_char = "##";
-    let snake_char = "[]";
-    let food_char = "()";
-    let empty_char = "  ";
-    let wall_collision_enabled = true; //If false, the snake will wrap around if it hits a wall
+    let start_size:i32= 3;
+    let wall_char:&str = "##";
+    let snake_char:&str = "[]";
+    let food_char:&str = "()";
+    let empty_char:&str = "  ";
+    let time_between_frames:u64 = 200; //in ms
+    let wall_collision_enabled:bool = true; //If false, the snake will wrap around if it hits a wall
     
 
     /*so here is my idea.
@@ -30,22 +30,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
 
     The drawing function can then easily turn that into a string, and print it.
     */
-    enable_raw_mode();
-    let mut is_alive:bool=true;
+    enable_raw_mode().ok();
     let mut board:[[i32;BOARD_SIZE[0]];BOARD_SIZE[1]] = [[0;BOARD_SIZE[0]];BOARD_SIZE[1]];
     draw_board(&board, &wall_char, &snake_char, &food_char, &empty_char);
-    let mut snake_pos:[usize;2] = [board.len()/2 as usize, board[0].len()/2 as usize];
+    let mut snake_pos:[i32;2] = [(board.len()/2) as i32, (board[0].len()/2) as i32];
     let mut snake_dir:[i32;2] = [0,1];
     let mut snake_len:i32 = start_size;
-    let mut food_pos:[usize;2] = [0,0];
+    let mut food_pos:[usize;2];
     let mut stdout = stdout();
     let mut rng = rand::thread_rng();
-    let mut score:i32 = 0;
+    let mut score:usize = 0;
     food_pos = [rng.gen_range(0..board.len()), rng.gen_range(0..board[0].len())];
     board[food_pos[0]][food_pos[1]] = -1;
-    while is_alive{
+    clear_screen();
+    loop{
         //Get input
-        let stdin = io::stdin();
+        //let stdin = io::stdin();
         //clear events
         /*while event::poll(std::time::Duration::from_millis(1))? {
             if let Event::Key(_) = event::read()? {
@@ -54,15 +54,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
         draw_board(&board, &wall_char, &snake_char, &food_char, &empty_char);
         print!("Score: {}", score);
         stdout.flush()?;
-        enable_raw_mode();
+        enable_raw_mode().ok();
         //wait
-        std::thread::sleep(std::time::Duration::from_millis(200));
+        std::thread::sleep(std::time::Duration::from_millis(time_between_frames));
         if event::poll(std::time::Duration::from_millis(1))? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
-                    KeyCode::Char('q') => {
-                        is_alive = false;
-                        disable_raw_mode();
+                    KeyCode::Char('q') | KeyCode::Esc  => {
+                        disable_raw_mode().ok();
                         break;
                     },
                     KeyCode::Left => {
@@ -81,48 +80,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
                 }
             }
         }
-        disable_raw_mode();
+        disable_raw_mode().ok();
         //Move snake
-        snake_pos[0] = (snake_pos[0] as i32 + snake_dir[0]) as usize;
-        snake_pos[1] = (snake_pos[1] as i32 + snake_dir[1]) as usize;
+        snake_pos[0] = (snake_pos[0] as i32 + snake_dir[0]) as i32;
+        snake_pos[1] = (snake_pos[1] as i32 + snake_dir[1]) as i32;
         //Check for collisions
-        if board[snake_pos[0]][snake_pos[1]] > 0{
-            is_alive = false;
-        }
         if wall_collision_enabled{
-            if snake_pos[0] >= board.len() || snake_pos[1] >= board[0].len(){
-                is_alive = false;
-                break;
+            if snake_pos[0] >= board.len() as i32 || snake_pos[1] >= board[0].len() as i32 || snake_pos[0] < 0 || snake_pos[1] < 0{
+                death_screen(score);
             }
-        }
-        else{
-            if snake_pos[0] >= board.len(){
+        }else{
+            if snake_pos[0] >= board.len() as i32{
                 snake_pos[0] = 0;
             }
-            if snake_pos[1] >= board[0].len(){
+            if snake_pos[1] >= board[0].len() as i32{
                 snake_pos[1] = 0;
             }
             if snake_pos[0] < 0{
-                snake_pos[0] = board.len()-1;
+                snake_pos[0] = (board.len()-1) as i32;
             }
             if snake_pos[1] < 0{
-                snake_pos[1] = board[0].len()-1;
+                snake_pos[1] = (board[0].len()-1) as i32;
             }
         }
+        if board[snake_pos[0] as usize][snake_pos[1] as usize] > 0{
+            death_screen(score);
+        }
+
+       
         
         //Check for food
-        if snake_pos==food_pos{
+        if snake_pos[0] as i32 == food_pos[0] as i32 && snake_pos[1] as i32 == food_pos[1] as i32{
             snake_len += 1;
             score += 1;
             food_pos = [rng.gen_range(0..board.len()), rng.gen_range(0..board[0].len())];
-            let mut i = 0;
+            let mut i:usize = 0;
+            let mut is_win:bool = false;
             while board[food_pos[0]][food_pos[1]] != 0{
                 if i > 10000{
-                    is_alive = false;
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
+                    println!();
                     println!("Youre win!");
+                    is_win = true;
                     break;
                 }
+            
+                i+=1;
                 food_pos = [rng.gen_range(0..board.len()), rng.gen_range(0..board[0].len())];
+            }
+            if is_win{
+                break;
             }
             board[food_pos[0]][food_pos[1]] = -1;
         }
@@ -134,7 +141,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
                 }
             }
         }
-        board[snake_pos[0]][snake_pos[1]] = snake_len;
+        board[snake_pos[0] as usize][snake_pos[1] as usize] = snake_len;
 
     }Ok(())
 
@@ -144,7 +151,7 @@ fn draw_board(board: &[[i32;BOARD_SIZE[0]];BOARD_SIZE[1]], wall_char: &str, snak
     
 
     //Draw the top wall
-    clear_screen();
+    cursor_to_top();
     println!("{}", wall_char.repeat(board[0].len()+2));
     for row in board.iter(){
         print!("{}", wall_char);
@@ -163,4 +170,16 @@ fn draw_board(board: &[[i32;BOARD_SIZE[0]];BOARD_SIZE[1]], wall_char: &str, snak
 
 fn clear_screen(){
     print!("\x1B[2J\x1B[1;1H");
+}
+
+fn cursor_to_top(){
+    print!("\x1B[1;1H");
+}
+
+fn death_screen(score:usize){
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    clear_screen();
+    println!("You died!");
+    println!("Final score: {}", score);
+    std::process::exit(0);
 }
